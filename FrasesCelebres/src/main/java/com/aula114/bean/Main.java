7ad2964da5b19d4a0be89a8c819e595bd00dd29b
Carmen Rufo <carmen.ruf.pal@gmail.com>
2020-05-12 14:28:48 +0200

    Modificando Main para uso vía escritorio

--- FrasesCelebres/src/main/java/com/aula114/bean/Main.java
+++ FrasesCelebres/src/main/java/com/aula114/bean/Main.java
@@ -1,30 +1,28 @@
 package com.aula114.bean;
 
 import java.util.Scanner;
+import java.util.Set;
+import java.util.List;
+
+import com.aula114.bean.ServiceFrase;
+import com.aula114.model.FraseCelebre;
+
 
 public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Bienvenido al registro de frases celebres\nElija entre las siguientes categorias:\n" + ViewManager.getListaCategorias());
+		ServiceFrase service = new ServiceFrase();
+		Set<String> set = service.mostrarCategorias();
+		System.out.println("Bienvenido al registro de frases celebres\nElija entre las siguientes categorias:\n" + set);
 		String tipo = sc.nextLine().toLowerCase();
 
-		switch(tipo) {
-			case "perdonar":
-				ServiceFrase.mostrarFrasesPorCategoria(tipo);
-			case "perseverancia":
-				ServiceFrase.mostrarFrasesPorCategoria(tipo);
-			case "ilusiones":
-				ServiceFrase.mostrarFrasesPorCategoria(tipo);
-			case "risas":
-				ServiceFrase.mostrarFrasesPorCategoria(tipo);
-			case "entusiasmo":
-				ServiceFrase.mostrarFrasesPorCategoria(tipo);
-			case "destino":
-				ServiceFrase.mostrarFrasesPorCategoria(tipo);
-			case "bondad":
-				ServiceFrase.mostrarFrasesPorCategoria(tipo);
-			case "sufrimiento":
-
+		if (set.contains(tipo)) {
+			List<FraseCelebre> lista = service.mostrarFrasesPorCategoria(tipo);
+			for(FraseCelebre unafrase : lista) {
+				System.out.println("*** " + unafrase.getFrase());
+			}
+		} else {
+			System.out.println("Lo siento, la categoría indicada no existe");
 		}
 	}	
 }
