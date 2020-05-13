4180697f6106221779fa756f00cf85abcf232b34
Anekane <carmen.ruf.pal@gmail.com>
2020-05-13 11:33:05 +0200

    Completando Main para su funcionalidad

--- FrasesCelebres/src/main/java/com/aula114/bean/Main.java
+++ FrasesCelebres/src/main/java/com/aula114/bean/Main.java
@@ -14,32 +14,44 @@ public class Main {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Dime que deseas buscar, autores o categorias.");
 		String buscar = sc.nextLine();
+
 		if(buscar.equalsIgnoreCase("categorias")){
-
-		Set<String> set = service.mostrarCategorias();
-		System.out.println("Bienvenido al registro de frases celebres\nElija entre las siguientes categorias:\n" + set);
-		String tipo = sc.nextLine().toLowerCase();
-		if (set.contains(tipo)) {
-			List<FraseCelebre> lista = service.mostrarFrasesPorCategoria(tipo);
-			for(FraseCelebre unafrase : lista) {
-				System.out.println("*** " + unafrase.getFrase() + "\n   ***    " + unafrase.getAutor().getNombre());
+			Set<String> setCat = service.mostrarCategorias();
+			System.out.println("Bienvenido al registro de frases celebres\nElija entre las siguientes categorias:\n" + setCat);
+			String tipo = sc.nextLine().toLowerCase();
+			if (setCat.contains(tipo)) {
+				List<FraseCelebre> listaCat = service.mostrarFrasesPorCategoria(tipo);
+				for(FraseCelebre unaFrase : listaCat) {
+					System.out.println("*** " + unaFrase.getFrase() + "\n   ***    " + unaFrase.getAutor().getNombre());
+				}
+			} else {
+				System.out.println("Lo siento, la categoría indicada no existe");
 			}
-		} else {
-			System.out.println("Lo siento, la categoría indicada no existe");
-		}
+		} else if(buscar.equalsIgnoreCase("autores")){
+			Set<String> setAut = service.mostrarAutores();
+			System.out.println("Bienvenido al registro de frases celebres\nElija entre las siguientes Autores:\n" + setAut);
+			String autor = sc.nextLine().toLowerCase();
 
-		}else if(buscar.equalsIgnoreCase("autores")){
-			Set<String> set1 = service.mostrarAutores();
-		System.out.println("Bienvenido al registro de frases celebres\nElija entre las siguientes Autores:\n" + set1);
-		String autor = sc.nextLine().toLowerCase();
-		if (set1.contains(autor)) {
-			List<FraseCelebre> lista1 = service.mostrarFrasesPorAutor(autor);
-			for(FraseCelebre unafrase : lista1) {
-				System.out.println("*** " + unafrase.getFrase() + "\n   ***    " + unafrase.getAutor().getNombre());
+			char[] convertir = autor.toCharArray();
+			autor = "";
+       		for(int i = 0; i < convertir.length; i++) {
+       			if(convertir[i] == convertir[0]){
+    		      convertir[i] = Character.toUpperCase(convertir[i]);
+           		}
+	           if(convertir[i] == ' '){
+    		      convertir[i+1] = Character.toUpperCase(convertir[i+1]);
+           		}
+           		autor = autor + convertir[i];
+       		}
+
+			if (setAut.contains(autor)) {
+				List<FraseCelebre> listaAut = service.mostrarFrasesPorAutor(autor);
+				for(FraseCelebre unaFrase : listaAut) {
+					System.out.println("*** " + unaFrase.getFrase() + "\n    *** " + unaFrase.getAutor().getNombre());
+				}
+			} else {
+				System.out.println("Lo siento, el autor indicado no existe");
 			}
-		} else {
-			System.out.println("Lo siento, el autor indicado no existe");
-		}
 
 		}
 		
